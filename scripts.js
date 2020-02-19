47770b294eb5d005f9f5b53141852c6394e76d8e
Daniel Puiatti <dpuiatti@oct.ca>
2020-02-19 16:31:05 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -4,52 +4,58 @@ const textToConvert = document.querySelector("#editor > div.ql-editor");
 let formattedWithHtml = "";
 let outputHTML = document.querySelector("#output");
 
- const formatWithHTMLfunction = () => {
-   formattedWithHtml = textToConvert.innerHTML
+const formatWithHTMLfunction = () => {
+  formattedWithHtml = textToConvert.innerHTML
 
-   //replace double space
-     .replace(/  /g, " ")
+    //replace double space
+    .replace(/  /g, " ")
 
-   //replace double space after period
-     .replace(/\.  /g, "\.")
-    
-   //wrap paragraphs in p tags
-     .replace(/(^[A-Z].*\.$)/gm, "<p>$1</p>")
-    
+    //replace double space after period
+    .replace(/\.  /g, ".")
+
+    //remove empty NBSP
+    .replace(/<p>&nbsp;<\/p>/g, "")
+
+    //wrap paragraphs in p tags
+    .replace(/(^[A-Z].*\.$)/gm, "<p>$1</p>")
+
+    //replace MS word paragraphs with lists
+    .replace(/<p>\u00B7&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g, "<li>")
+
     //encode urls
     //  .replace(
     //    /([-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&=]*))/gm,
     //    "<a href='http:$1'>$1</a>"
     //  )
-    
-     //words to cite
-     .replace(/Globe and Mail/g, "<cite>Globe and Mail</cite>")
-     .replace(/The Financial Post/g, "<cite>The Financial Post</cite>")
-    
-     //custom list of words to format
-     .replace(
-       /Professionally Speaking/g,
-       "<a href='http:professionallyspeaking.oct.ca'><cite>Professionally Speaking</cite></a>"
-     )
-     .replace(
-       /Pour parler profession/g,
-       "<a href='http:pourparlerprofession.oeeo.ca'><cite>Pour parler profession</cite></a>"
-     )
-    
-     //french replacements
-       //mme
-         .replace(/Mme /g, "M<sup>me</sup>&nbsp;")
-       //dre
-         .replace(/Dre /g, "D<sup>re</sup>&nbsp;")
-       //superscript numbers
-         .replace(/(\d)(e)/g, "$1&nbsp<sup>e</sup>")
-       //oeuvres
-         .replace(/oeuvre/g, "œuvre")
-       //soeur
-         .replace(/soeur/g, "sœur")
 
-   return formattedWithHtml;
- };
+    //words to cite
+    .replace(/Globe and Mail/g, "<cite>Globe and Mail</cite>")
+    .replace(/The Financial Post/g, "<cite>The Financial Post</cite>")
+
+    //custom list of words to format
+    .replace(
+      /Professionally Speaking/g,
+      "<a href='http:professionallyspeaking.oct.ca'><cite>Professionally Speaking</cite></a>"
+    )
+    .replace(
+      /Pour parler profession/g,
+      "<a href='http:pourparlerprofession.oeeo.ca'><cite>Pour parler profession</cite></a>"
+    )
+
+    //french replacements
+    //mme
+    .replace(/Mme /g, "M<sup>me</sup>&nbsp;")
+    //dre
+    .replace(/Dre /g, "D<sup>re</sup>&nbsp;")
+    //superscript numbers
+    .replace(/(\d)(e)/g, "$1&nbsp<sup>e</sup>")
+    //oeuvres
+    .replace(/oeuvre/g, "œuvre")
+    //soeur
+    .replace(/soeur/g, "sœur");
+
+  return formattedWithHtml;
+};
 
 submitButton.addEventListener("click", () => {
   formatWithHTMLfunction();
