3a6840929ccf06cb07acf8a01864bbb2b3431e0f
Daniel Puiatti <dpuiatti@oct.ca>
2020-03-31 13:15:38 -0400

    changes

--- scripts.js
+++ scripts.js
@@ -6,8 +6,46 @@ let outputHTML = document.querySelector("#output");
 var language;
 
 const formatWithHTMLfunctionEN = () => {
-  formattedWithHtml = textToConvert.innerHTML
+  formattedWithHtml = textToConvert.innerHTML;
 
+  generalFormatting();
+  return formattedWithHtml;
+};
+
+const formatWithHTMLfunctionFR = () => {
+  formattedWithHtml = textToConvert.innerHTML
+    //french replacements
+    //mme
+    .replace(/Mme /g, "M<sup>me</sup>&nbsp;")
+    //mme w/sup
+    .replace(/M\<sup\>me\<\/sup> /g, "M<sup>me</sup>&nbsp;")
+    //m.
+    .replace(/M\. /g, "M.&nbsp;")
+    //numbers
+    .replace(
+      /([0-9]|[0-9][0-9]|[0-9][0-9][0-9]) ([0-9][0-9][0-9])/g,
+      "$1&nbsp;$2"
+    )
+    //after colon
+    .replace(/( \: )/g, "&nbsp;:&nbsp;")
+    //dre
+    .replace(/Dre /g, "D<sup>re</sup>&nbsp;")
+    //superscript numbers
+    .replace(/(\d)(e)/g, "$1&nbsp<sup>e</sup>")
+    //oeuvres
+    .replace(/oeuvre/g, "&‌OElig;uvre")
+    //soeur
+    .replace(/soeur/g, "s&‌OElig;ur")
+    //apostrophe with curly
+    .replace(/[a-z]'[a-z]/g, "&rsquo;");
+
+  generalFormatting();
+
+  return formattedWithHtml;
+};
+
+const generalFormatting = () => {
+  formattedWithHtml = textToConvert.innerHTML
     //replace MS word paragraphs with lists
     .replace(
       /<p>\u00B7&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g,
@@ -44,57 +82,6 @@ const formatWithHTMLfunctionEN = () => {
       /Ontario College of Teachers Act/g,
       "<cite>Ontario College of Teachers Act</cite>"
     )
-
-    //custom list of words to format
-    .replace(
-      / Professionally Speaking /g,
-      '<a href="http:professionallyspeaking.oct.ca"><cite>Professionally Speaking</cite></a>'
-    )
-    .replace(
-      / Pour parler profession /g,
-      '<a href="http:pourparlerprofession.oeeo.ca"><cite>Pour parler profession</cite></a>'
-    )
-    .replace(
-      /Margaret Wilson Library/g,
-      '<a href="https://www.oct.ca/members/services/library">Margaret Wilson Library</a>'
-    );
-
-  return formattedWithHtml;
-};
-
-const formatWithHTMLfunctionFR = () => {
-  formattedWithHtml = textToConvert.innerHTML
-
-    //replace MS word paragraphs with lists
-    .replace(
-      /<p>\u00B7&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g,
-      "<li>"
-    )
-    //replace double space
-    .replace(/  /g, " ")
-
-    //replace double space after period
-    .replace(/\.  /g, ".")
-
-    //remove <p>&nbsp;</p>
-    .replace(/<p>&nbsp;<\/p>/g, "")
-
-    //remove <span> tag
-    .replace(/<span[^>]*>/g, "")
-
-    //remove </span> tag
-    .replace(/<\/span[^>]*>/g, "")
-
-    //remove style tags
-    .replace(/style=".*;"/g, "")
-
-    //remove <p><br></p>
-    .replace(/<p><br><\/p>/g, "")
-
-    //remove <strong>&nbsp;</strong>
-    .replace(/<strong>&nbsp;<\/strong>/g, "")
-
-    //words to cite
     .replace(
       /Loi sur l’Ordre des enseignantes et des enseignants de l’Ontario/g,
       "<cite>Loi sur l’Ordre des enseignantes et des enseignants de l’Ontario</cite>"
@@ -113,33 +100,7 @@ const formatWithHTMLfunctionFR = () => {
       /Margaret Wilson Library/g,
       '<a href="https://www.oct.ca/members/services/library">Margaret Wilson Library</a>'
     )
-
-    //french replacements
-    //mme
-    .replace(/Mme /g, "M<sup>me</sup>&nbsp;")
-    //mme w/sup
-    .replace(/M\<sup\>me\<\/sup> /g, "M<sup>me</sup>&nbsp;")
-    //m.
-    .replace(/M\. /g, "M.&nbsp;")
-    //numbers
-    .replace(
-      /([0-9]|[0-9][0-9]|[0-9][0-9][0-9]) ([0-9][0-9][0-9])/g,
-      "$1&nbsp;$2"
-    )
-    //after colon
-    .replace(/( \: )/g, "&nbsp;:&nbsp;")
-    //dre
-    .replace(/Dre /g, "D<sup>re</sup>&nbsp;")
-    //superscript numbers
-    .replace(/(\d)(e)/g, "$1&nbsp<sup>e</sup>")
-    //oeuvres
-    .replace(/oeuvre/g, "&‌OElig;uvre")
-    //soeur
-    .replace(/soeur/g, "s&‌OElig;ur")
-    //apostrophe with curly
-    .replace(/[a-z]'[a-z]/g, "&rsquo;");
-
-  return formattedWithHtml;
+    .replace(/target="_blank"/g,"");
 };
 
 submitButton.addEventListener("click", () => {
