41cf1fef1887e71c7535343d641c3c67398adaf9
Daniel Puiatti <dpuiatti@oct.ca>
2021-09-01 11:30:29 -0400

    made the output HTML

--- scripts.js
+++ scripts.js
@@ -2,6 +2,6 @@ document.getElementById("submitButton").addEventListener("click", function() {
     let submitButton = document.querySelector("#submitButton");
     let input = CKEDITOR.instances.editor.getData();
     let output = document.querySelector("#output");
-    output.innerHTML = input;
+    output.innerText = input;
 
 });
