bade0db01790283ada96395a70d8e5b3c44889d1
Daniel Puiatti <dpuiatti@oct.ca>
2021-09-02 11:24:59 -0400

    added clear styling, remove blanks and copy HTML functions

--- scripts.js
+++ scripts.js
@@ -1,7 +1,45 @@
 document.getElementById("submitButton").addEventListener("click", function() {
     let submitButton = document.querySelector("#submitButton");
+    let clearStyle = document.querySelector("#clearStyles");
+    let removeBlanks = document.querySelector("#removeBlanks");
+    let copyToClipboad = document.querySelector("#copyHTML");
     let input = CKEDITOR.instances.editor.getData();
     let output = document.querySelector("#output");
     output.innerText = input;
 
+    //events for buttons
+
+    //clear styles
+    clearStyle.addEventListener("click", function() {
+        let clearStylesAction = output.innerText
+            .replaceAll("<strong>", "")
+            .replaceAll("</strong>", "")
+            .replaceAll("<em>", "")
+            .replaceAll("</em>", "");
+        output.innerText = "";
+        output.innerText = clearStylesAction;
+    });
+
+    //remove blanks
+    removeBlanks.addEventListener("click", function() {
+        let clearBlanksAction = output.innerText
+            .replaceAll("<p>&nbsp;</p>", "")
+            .replaceAll(
+                "<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </p>",
+                ""
+            )
+            .replaceAll("<br>", "");
+        output.innerText = "";
+        output.innerText = clearBlanksAction;
+    });
+
+
+    //copy to clipboard
+    copyToClipboad.addEventListener("click", function() {
+        /* Copy the text inside the text field */
+        navigator.clipboard.writeText(output.innerText);
+
+        /* Alert the copied text */
+        alert("Copied the text: " + output.innerText);
+    });
 });
